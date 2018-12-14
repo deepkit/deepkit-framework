82e8518773f4f1e865cf685e231ceedc871d9863
Marc J. Schmidt <marc@marcjschmidt.de>
2018-12-14 19:15:11 +0100

    added first implementation for getResolvedReflection with deep property path

--- tests/plain-to.spec.ts
+++ tests/plain-to.spec.ts
@@ -1,8 +1,64 @@
 import 'jest-extended'
 import 'reflect-metadata';
-import {SimpleModel, SubModel} from "./entities";
-import {partialPlainToClass, plainToClass} from "../src/mapper";
+import {Plan, SimpleModel, SubModel} from "./entities";
+import {getResolvedReflection, partialPlainToClass, plainToClass} from "../src/mapper";
 import {EnumType} from "..";
+
+test('getResolvedReflection simple', () => {
+    expect(getResolvedReflection(SimpleModel, 'id')!.resolvedClassType).toBe(SimpleModel);
+    expect(getResolvedReflection(SimpleModel, 'id')!.resolvedPropertyName).toBe('id');
+    expect(getResolvedReflection(SimpleModel, 'id')!.type).toBe('uuid');
+    expect(getResolvedReflection(SimpleModel, 'id')!.typeValue).toBeNull();
+    expect(getResolvedReflection(SimpleModel, 'id')!.array).toBe(false);
+    expect(getResolvedReflection(SimpleModel, 'id')!.map).toBe(false);
+
+    expect(getResolvedReflection(SimpleModel, 'plan')!.resolvedClassType).toBe(SimpleModel);
+    expect(getResolvedReflection(SimpleModel, 'plan')!.resolvedPropertyName).toBe('plan');
+    expect(getResolvedReflection(SimpleModel, 'plan')!.type).toBe('enum');
+    expect(getResolvedReflection(SimpleModel, 'plan')!.typeValue).toBe(Plan);
+    expect(getResolvedReflection(SimpleModel, 'plan')!.array).toBe(false);
+    expect(getResolvedReflection(SimpleModel, 'plan')!.map).toBe(false);
+
+    expect(getResolvedReflection(SimpleModel, 'children')!.resolvedClassType).toBe(SimpleModel);
+    expect(getResolvedReflection(SimpleModel, 'children')!.resolvedPropertyName).toBe('children');
+    expect(getResolvedReflection(SimpleModel, 'children')!.type).toBe('class');
+    expect(getResolvedReflection(SimpleModel, 'children')!.typeValue).toBe(SubModel);
+    expect(getResolvedReflection(SimpleModel, 'children')!.array).toBe(true);
+    expect(getResolvedReflection(SimpleModel, 'children')!.map).toBe(false);
+
+    expect(getResolvedReflection(SimpleModel, 'childrenMap')!.resolvedClassType).toBe(SimpleModel);
+    expect(getResolvedReflection(SimpleModel, 'childrenMap')!.resolvedPropertyName).toBe('childrenMap');
+    expect(getResolvedReflection(SimpleModel, 'childrenMap')!.type).toBe('class');
+    expect(getResolvedReflection(SimpleModel, 'childrenMap')!.typeValue).toBe(SubModel);
+    expect(getResolvedReflection(SimpleModel, 'childrenMap')!.array).toBe(false);
+    expect(getResolvedReflection(SimpleModel, 'childrenMap')!.map).toBe(true);
+});
+
+test('getResolvedReflection deep', () => {
+    expect(getResolvedReflection(SimpleModel, 'children.0.label')!.resolvedClassType).toBe(SubModel);
+    expect(getResolvedReflection(SimpleModel, 'children.0.label')!.resolvedPropertyName).toBe('label');
+    expect(getResolvedReflection(SimpleModel, 'children.0.label')!.type).toBe('string');
+    expect(getResolvedReflection(SimpleModel, 'children.0.label')!.typeValue).toBeNull();
+    expect(getResolvedReflection(SimpleModel, 'children.0.label')!.array).toBe(false);
+    expect(getResolvedReflection(SimpleModel, 'children.0.label')!.map).toBe(false);
+
+    expect(getResolvedReflection(SimpleModel, 'childrenMap.foo.label')!.resolvedClassType).toBe(SubModel);
+    expect(getResolvedReflection(SimpleModel, 'childrenMap.foo.label')!.resolvedPropertyName).toBe('label');
+    expect(getResolvedReflection(SimpleModel, 'childrenMap.foo.label')!.type).toBe('string');
+    expect(getResolvedReflection(SimpleModel, 'childrenMap.foo.label')!.typeValue).toBeNull();
+    expect(getResolvedReflection(SimpleModel, 'childrenMap.foo.label')!.array).toBe(false);
+    expect(getResolvedReflection(SimpleModel, 'childrenMap.foo.label')!.map).toBe(false);
+
+    expect(getResolvedReflection(SimpleModel, 'childrenMap.foo.unknown')).toBeNull();
+
+    expect(getResolvedReflection(SimpleModel, 'childrenMap.foo')!.resolvedClassType).toBe(SimpleModel);
+    expect(getResolvedReflection(SimpleModel, 'childrenMap.foo')!.resolvedPropertyName).toBe('childrenMap');
+    expect(getResolvedReflection(SimpleModel, 'childrenMap.foo')!.type).toBe('class');
+    expect(getResolvedReflection(SimpleModel, 'childrenMap.foo')!.typeValue).toBe(SubModel);
+    expect(getResolvedReflection(SimpleModel, 'childrenMap.foo')!.array).toBe(false);
+    expect(getResolvedReflection(SimpleModel, 'childrenMap.foo')!.map).toBe(false);
+});
+
 
 test('plain-to test simple model', () => {
 
