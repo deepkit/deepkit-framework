e370ee730e536a1d84b1e5f68314f7b3697e7de7
Marc J. Schmidt <marc@marcjschmidt.de>
2019-02-07 17:03:53 +0100

    added observable support (without mapping)

--- start.ts
+++ start.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import {ApplicationServer} from "./src/application-server";
 import {Action, ApplicationModule, Controller} from "./src/decorators";
 import {Application, Session} from "./src/application";
+import {Observable} from "rxjs";
 
 @Controller('user')
 class UserController {
@@ -9,6 +10,33 @@ class UserController {
     @Action()
     name(): string {
         return "this is a name";
+    }
+
+    @Action()
+    bla(): Observable<string> {
+        return new Observable((observer) => {
+            console.log('bla subscribe');
+
+            let i = 0;
+            const internal = setInterval(() => {
+                if (i > 10) {
+                    observer.complete();
+                    clearInterval(internal);
+                    return;
+                }
+
+                const next = String(i++);
+                console.log('next', next);
+                observer.next(next);
+            }, 1000);
+
+            return {
+                unsubscribe(): void {
+                    console.log('bla unsubscribe');
+                    clearInterval(internal);
+                }
+            }
+        });
     }
 }
 
@@ -21,7 +49,6 @@ class MyApp extends Application {
         await super.bootstrap();
         console.log('bootstrapped =)');
     }
-
 
     async authenticate(token: any): Promise<Session> {
         console.log('authenticate', token);
