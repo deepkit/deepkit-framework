e5987c39b6983f5a6b7f710bd045a8f2a546d4c9
Marc J. Schmidt <marc@marcjschmidt.de>
2019-03-11 17:30:00 +0100

    added Timer class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export * from './rxjs';
+export * from './timer';
---
+++ src/timer.ts
@@ -0,0 +1,20 @@
+import {Timer} from "@marcj/estdlib";
+import {Observer} from "rxjs";
+
+/**
+ * Allows to create Timer which get deactivated automatically
+ * when the observer is stopped.
+ */
+export class ObserverTimer extends Timer {
+    constructor(protected observer: Observer<any>) {
+        super();
+    }
+
+    setTimeout(cb: () => void, timeout: number): any {
+        super.setTimeout(() => {
+            if (!this.observer.closed) {
+                cb();
+            }
+        }, timeout);
+    }
+}
