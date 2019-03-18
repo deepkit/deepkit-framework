51ae40aa7711574092629833b011a75d9c3a6bc0
Marc J. Schmidt <marc@marcjschmidt.de>
2019-03-18 21:50:49 +0100

    added benchmark including class-transformer
    fixed nest package readme

---
+++ bench.spec.ts
@@ -0,0 +1,56 @@
+import 'jest';
+import {classToPlain, Field, plainToClass} from "../core";
+import {plainToClass as classTransformerPlainToClass, classToPlain as classTransformerClassToPlain} from "class-transformer";
+import {bench} from "./util";
+
+
+export class MarshalSuperSimple {
+    constructor(
+        @Field() public id: number,
+        @Field() public name: string
+    ) {}
+}
+
+export class ClassTransformerSuperSimple {
+    public id?: number;
+    public name?: string;
+}
+
+test('benchmark plainToClass', () => {
+    bench('Marshal: 10000x plainToClass SuperSimple', () => {
+        for (let i = 0; i < 10000; i++) {
+            plainToClass(MarshalSuperSimple, {
+                name: 'name' + i,
+                id: i
+            });
+        }
+    });
+
+    bench('ClassTransformer: 10000x plainToClass SuperSimple', () => {
+        for (let i = 0; i < 10000; i++) {
+            classTransformerPlainToClass(ClassTransformerSuperSimple, {
+                name: 'name' + i,
+                id: i
+            });
+        }
+    });
+});
+
+test('benchmark classToPlain', () => {
+    bench('Marshal: 10000x classToPlain SuperSimple', () => {
+        for (let i = 0; i < 10000; i++) {
+            const item = new MarshalSuperSimple(i, 'name' + i);
+            classToPlain(MarshalSuperSimple, item);
+        }
+    });
+
+    bench('ClassTransformer: 10000x classToPlain SuperSimple', () => {
+        for (let i = 0; i < 10000; i++) {
+            const item = new ClassTransformerSuperSimple;
+            item.id = i;
+            item.name = 'name' + i;
+
+            classTransformerClassToPlain(item);
+        }
+    });
+});
