d3b43f9a3301c6e40b17a6d8265ea61a5b630065
Marc J. Schmidt <marc@marcjschmidt.de>
2019-12-21 00:16:24 +0100

    added moment.js support

---
+++ tests/moment.spec.ts
@@ -0,0 +1,31 @@
+import 'jest-extended'
+import 'reflect-metadata';
+import * as moment from 'moment';
+import {getClassSchema, f, classToPlain, plainToClass} from "..";
+
+test('test moment', () => {
+    class Model {
+        @f.moment()
+        created: moment.Moment = moment();
+    }
+
+    const schema = getClassSchema(Model);
+    const prop = schema.getProperty('created');
+    expect(prop.type).toBe('moment');
+
+
+    const m = new Model;
+    m.created = moment(new Date('2018-10-13T12:17:35.000Z'));
+
+    const p = classToPlain(Model, m);
+    expect(p.created).toBeString();
+    expect(p.created).toBe('2018-10-13T12:17:35.000Z');
+
+    {
+        const m = plainToClass(Model, {
+            created: '2018-10-13T12:17:35.000Z'
+        });
+        expect(m.created).toBeInstanceOf(moment);
+        expect(m.created.toJSON()).toBe('2018-10-13T12:17:35.000Z' );
+    }
+});
