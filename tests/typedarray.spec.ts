d1842aa78228c88ec88fe5fe7e82b737978600d3
Marc J. Schmidt <marc@marcjschmidt.de>
2020-01-30 22:53:24 +0100

    added support for ArrayBuffer directly

--- tests/typedarray.spec.ts
+++ tests/typedarray.spec.ts
@@ -22,6 +22,7 @@ test('Int8Array', async () => {
 
     const mongo = classToMongo(Clazz, clazz);
     expect(mongo.ints).toBeInstanceOf(Binary);
+    expect(mongo.ints.toString('base64')).toBe('AUA=');
 
     const clazz3 = mongoToClass(Clazz, mongo);
     expect(clazz3.ints).toBeInstanceOf(Int8Array);
@@ -30,4 +31,71 @@ test('Int8Array', async () => {
 
     const mongo2 = plainToMongo(Clazz, plain);
     expect(mongo2.ints).toBeInstanceOf(Binary);
+    expect(mongo2.ints.toString('base64')).toBe(mongo.ints.toString('base64'))
+});
+
+test('arrayBuffer', async () => {
+    class Clazz {
+        @f ints?: ArrayBuffer;
+    }
+
+    const clazz = new Clazz();
+    clazz.ints = new Int8Array(2);
+    clazz.ints[0] = 1;
+    clazz.ints[1] = 64;
+
+    expect(getClassSchema(Clazz).getProperty('ints').type).toBe('arrayBuffer');
+    expect(new Int8Array(clazz.ints!)[0]).toBe(1);
+    expect(new Int8Array(clazz.ints!)[1]).toBe(64);
+
+    const plain = classToPlain(Clazz, clazz);
+    expect(plain.ints).toBeString();
+    expect(plain.ints).toBe('AUA=');
+
+    const mongo = classToMongo(Clazz, clazz);
+    expect(mongo.ints).toBeInstanceOf(Binary);
+    expect(mongo.ints.toString('base64')).toBe('AUA=');
+
+    const clazz2 = mongoToClass(Clazz, mongo);
+    expect(clazz2.ints).not.toBeInstanceOf(Int8Array);
+    expect(clazz2.ints).toBeInstanceOf(ArrayBuffer);
+    expect(new Int8Array(clazz2.ints!)[0]).toBe(1);
+    expect(new Int8Array(clazz2.ints!)[1]).toBe(64);
+
+    const mongo2 = plainToMongo(Clazz, plain);
+    expect(mongo2.ints).toBeInstanceOf(Binary);
+    expect(mongo2.ints.toString('base64')).toBe(mongo.ints.toString('base64'))
+});
+
+test('arrayBuffer 2', async () => {
+    class Clazz {
+        @f ints?: ArrayBuffer;
+    }
+
+    const clazz = new Clazz();
+    clazz.ints = new ArrayBuffer(2);
+    new Int8Array(clazz.ints)[0] = 1;
+    new Int8Array(clazz.ints)[1] = 64;
+
+    expect(getClassSchema(Clazz).getProperty('ints').type).toBe('arrayBuffer');
+    expect(new Int8Array(clazz.ints!)[0]).toBe(1);
+    expect(new Int8Array(clazz.ints!)[1]).toBe(64);
+
+    const plain = classToPlain(Clazz, clazz);
+    expect(plain.ints).toBeString();
+    expect(plain.ints).toBe('AUA=');
+
+    const mongo = classToMongo(Clazz, clazz);
+    expect(mongo.ints).toBeInstanceOf(Binary);
+    expect(mongo.ints.toString('base64')).toBe('AUA=');
+
+    const clazz2 = mongoToClass(Clazz, mongo);
+    expect(clazz2.ints).not.toBeInstanceOf(Int8Array);
+    expect(clazz2.ints).toBeInstanceOf(ArrayBuffer);
+    expect(new Int8Array(clazz2.ints!)[0]).toBe(1);
+    expect(new Int8Array(clazz2.ints!)[1]).toBe(64);
+
+    const mongo2 = plainToMongo(Clazz, plain);
+    expect(mongo2.ints).toBeInstanceOf(Binary);
+    expect(mongo2.ints.toString('base64')).toBe(mongo.ints.toString('base64'))
 });
