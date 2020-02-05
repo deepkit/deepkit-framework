a53a50810c32c0203ddf0d3f150f2892d0e0ea47
Marc J. Schmidt <marc@marcjschmidt.de>
2020-02-05 21:28:54 +0100

    better progress support

--- src/worker.ts
+++ src/worker.ts
@@ -56,6 +56,9 @@ export class Worker {
                                 });
                                 return true;
                             }
+                            bufferedAmount(): number {
+                                return ws.bufferedAmount;
+                            }
                         });
                     }
                 },
