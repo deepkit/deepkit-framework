31cab942f04cf4fe56d17d3f3dec0d4ef521f8a3
Marc J. Schmidt <marc@marcjschmidt.de>
2020-02-25 17:12:55 +0100

    added more lock benchmarks

--- tests/exchange.spec.ts
+++ tests/exchange.spec.ts
@@ -10,6 +10,8 @@ afterAll(async () => {
     closeCreatedExchange();
 });
 
+jest.setTimeout(10000);
+
 test('test basic', async () => {
     const server = new ExchangeServer('auto');
     await server.start();
@@ -150,15 +152,31 @@ test('test lock performance', async () => {
     const locker = await createExchange();
     const start = performance.now();
 
-    const count = 2_000;
+    const count = 10_000;
     for (let i = 0; i < count; i++) {
         const lock1 = await locker.lock('test-perf-' + i);
         await lock1.unlock();
     }
 
     //0.0035 takes native lock per item
-    //this takes 0.203, that's the price of communicating via webSockets
-    console.log(count, 'locks took', performance.now() - start, 'ms', (performance.now() - start) / count);
+    //this takes 0.102, that's the price of communicating via webSockets
+    console.log(count, 'sequential locks took', performance.now() - start, 'ms', count * (1000 / (performance.now() - start)), 'op/s');
+});
+
+test('test lock performance concurrent', async () => {
+    const locker = await createExchange();
+    const start = performance.now();
+
+    const count = 10_000;
+    const all: Promise<void>[] = [];
+    for (let i = 0; i < count; i++) {
+        all.push(locker.lock('test-perf-' + i).then((v) => {
+            return v.unlock();
+        }));
+    }
+
+    await Promise.all(all);
+    console.log(count, 'concurrent locks took', performance.now() - start, 'ms', count * (1000 / (performance.now() - start)), 'op/s');
 });
 
 
