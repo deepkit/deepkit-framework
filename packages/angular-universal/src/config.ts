86e3dcbbdaf9121057ce19db1451321dea0c08a4
Marc J. Schmidt <marc@marcjschmidt.de>
2021-01-12 19:52:11 +0100

    copyright update

--- packages/angular-universal/src/config.ts
+++ packages/angular-universal/src/config.ts
@@ -1,3 +1,13 @@
+/*
+ * Deepkit Framework
+ * Copyright (C) 2021 Deepkit UG
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the MIT License.
+ *
+ * You should have received a copy of the MIT License along with this program.
+ */
+
 import { createConfig } from '@deepkit/framework';
 import { t } from '@deepkit/type';
 
--- packages/angular-universal/src/module.ts
+++ packages/angular-universal/src/module.ts
@@ -1,3 +1,13 @@
+/*
+ * Deepkit Framework
+ * Copyright (C) 2021 Deepkit UG
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the MIT License.
+ *
+ * You should have received a copy of the MIT License along with this program.
+ */
+
 import { createModule } from '@deepkit/framework';
 import { AngularUniversalListener } from './listener';
 import { config } from './config';
