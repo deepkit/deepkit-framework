26f323e80d5fd52c81365606e4f6b0b530e1dda3
marcj <marc@marcjschmidt.de>
2021-02-14 01:47:09 +0100

    fix: typos

--- packages/framework-debug-gui/e2e/src/app.e2e-spec.ts
+++ packages/framework-debug-gui/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('framework-debug-gui2 app is running!');
+    expect(await page.getTitleText()).toEqual('framework-debug-gui app is running!');
   });
 
   afterEach(async () => {
--- packages/framework-debug-gui/karma.conf.js
+++ packages/framework-debug-gui/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/framework-debug-gui2'),
+      dir: require('path').join(__dirname, './coverage/framework-debug-gui'),
       subdir: '.',
       reporters: [
         { type: 'html' },
