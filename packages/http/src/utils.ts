58e560ef3bf1a7bb74ecf3942fb742a6a538e277
Trevor Gau <trgau@microsoft.com>
2021-06-25 11:12:30 -0400

    Formatting

--- packages/http/src/utils.ts
+++ packages/http/src/utils.ts
@@ -9,8 +9,8 @@ export function normalizeDirectory(path: string): string {
 export function resolveUrl(from: string, to: string) {
     const resolvedUrl = new URL(to, new URL(from, 'resolve://'));
     if (resolvedUrl.protocol === 'resolve:') {
-      const { pathname, search, hash } = resolvedUrl;
-      return pathname + search + hash;
+        const { pathname, search, hash } = resolvedUrl;
+        return pathname + search + hash;
     }
     return resolvedUrl.toString();
-  }
+}
