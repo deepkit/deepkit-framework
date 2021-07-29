62d49f3a3640c4d1d7756b7e020c1a2c9327e640
marcj <marc@marcjschmidt.de>
2021-07-30 00:26:43 +0200

    chore(ci): typo

--- packages/mongo/tests/factory.ts
+++ packages/mongo/tests/factory.ts
@@ -3,7 +3,7 @@ import { DatabaseFactory } from '@deepkit/orm-integration';
 import { MongoDatabaseAdapter } from '../src/adapter';
 
 export const databaseFactory: DatabaseFactory = async (entities): Promise<Database> => {
-    const adapter = new MongoDatabaseAdapter('mongodb://localhost:27018/orm-integration');
+    const adapter = new MongoDatabaseAdapter('mongodb://localhost/orm-integration');
 
     const database = new Database(adapter);
     if (entities) database.registerEntity(...entities);
