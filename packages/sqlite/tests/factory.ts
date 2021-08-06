92a927d201a1750fc9508c707acc9c3d091ccbfc
marcj <marc@marcjschmidt.de>
2021-08-06 02:16:24 +0200

    fix(sqlite): tests use each own sqlite database

--- packages/sqlite/tests/factory.ts
+++ packages/sqlite/tests/factory.ts
@@ -1,9 +1,11 @@
 import { Database } from '@deepkit/orm';
 import { DatabaseFactory } from '@deepkit/orm-integration';
 import { SQLiteDatabaseAdapter } from '../src/sqlite-adapter';
+import { mkdtempSync } from 'fs';
+import { join } from 'path';
 
 export const databaseFactory: DatabaseFactory = async (entities): Promise<Database> => {
-    const adapter = new SQLiteDatabaseAdapter('/tmp/testing.sqlite');
+    const adapter = new SQLiteDatabaseAdapter(join(mkdtempSync('/tmp/', 'utf8'), 'db.sqlite'));
 
     const database = new Database(adapter);
     if (entities) database.registerEntity(...entities);
