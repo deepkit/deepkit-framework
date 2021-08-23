4394c7aa0d64b363d5c0b6ae5d4e6123ef4b8eca
marcj <marc@marcjschmidt.de>
2021-08-23 02:46:06 +0200

    chore(orm-browser): use new loadConfigFromEnv
    Find orm-browser-gui in more setups.

--- packages/orm-browser/main.ts
+++ packages/orm-browser/main.ts
@@ -25,9 +25,9 @@ const appModule = new AppModule({
         })
     ]
 }).setup((module, config) => {
-    const localPath = findParentPath('orm-browser-gui/dist/orm-browser-gui');
-    if (!localPath) throw new Error('orm-browser-gui not installed');
+    const localPath = findParentPath('node_modules/@deepkit/orm-browser-gui/dist/orm-browser-gui', __dirname);
+    if (!localPath) throw new Error('node_modules/@deepkit/orm-browser-gui not installed in ' + __dirname);
     registerStaticHttpController(module, '/', localPath);
 });
 
-new Application(appModule).run(['server:start']);
+new Application(appModule).loadConfigFromEnv({prefix: 'ORM_BROWSER_'}).run(['server:start']);
