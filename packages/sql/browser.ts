0920e6667517bbb775f20f009219f8c271b0b97f
marcj <marc@marcjschmidt.de>
2021-08-29 22:57:29 +0200

    feature(sql): barrel for browser build

---
+++ packages/sql/browser.ts
@@ -0,0 +1,19 @@
+/*
+ * Deepkit Framework
+ * Copyright (C) 2021 Deepkit UG, Marc J. Schmidt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the MIT License.
+ *
+ * You should have received a copy of the MIT License along with this program.
+ */
+
+export * from './src/sql-builder';
+export * from './src/sql-adapter';
+export * from './src/sql-filter-builder';
+
+export * from './src/schema/table';
+export * from './src/reverse/schema-parser';
+export * from './src/platform/default-platform';
+export * from './src/serializer/sql-serializer';
+
