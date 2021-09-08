a8913ea08fa0fe24c239d481efa49c15c34a2a81
marcj <marc@marcjschmidt.de>
2021-09-08 06:05:55 +0200

    feature(type): extract more types in transformer and support ts-node

--- packages/type/install-transformer.ts
+++ packages/type/install-transformer.ts
@@ -1,42 +1,50 @@
+#!/usr/bin/env node
+
 /**
  * This script installs the deepkit/type transformer (that extracts automatically types and adds the correct @t decorator) to the typescript node_modules.
  *
  * The critical section that needs adjustment is in the `function getScriptTransformers` in `node_modules/typescript/lib/tsc.js`.
  */
 
-//@ts-ignore
 import { dirname, join, relative } from 'path';
-//@ts-ignore
 import { readFileSync, writeFileSync } from 'fs';
+import { cwd } from 'process';
 
-function getCode(deepkitDistPath: string): string {
+function getCode(deepkitDistPath: string, varName: string): string {
     return `
         var typeTransformer = require('${deepkitDistPath}/src/transformer');
         if (typeTransformer) {
-            if (!customTransformers) customTransformers = {}
-            if (!customTransformers.before) customTransformers.before = []
-            customTransformers.before.push(typeTransformer.transformer);
+            if (!${varName}) ${varName} = {}
+            if (!${varName}.before) ${varName}.before = []
+            ${varName}.before.push(typeTransformer.transformer);
         }
     `;
 }
 
-function injectTransformer(deepkitDistPath: string, code: string): string {
+function injectTransformerForTsc(deepkitDistPath: string, code: string): string {
     if (code.includes(deepkitDistPath)) return code;
 
-    return code.replace(/function getScriptTransformers\([^)]+\)\s*{/, function (a) {
-        return a + '\n    ' + getCode(deepkitDistPath);
+    code = code.replace(/function getScriptTransformers\([^)]+\)\s*{/, function (a) {
+        return a + '\n    ' + getCode(deepkitDistPath, 'customTransformers');
     });
+
+    return code;
 }
 
-//@ts-ignore
-const typeScriptPath = dirname(require.resolve('typescript'));
-//@ts-ignore
-const deepkitDistPath = relative(typeScriptPath, __dirname);
-// console.log('typeScriptPath', typeScriptPath);
-// console.log('deepkitDistPath', deepkitDistPath);
-// console.log('code', injectTransformer(deepkitDistPath, code));
+function injectTransformerForTypescript(deepkitDistPath: string, code: string): string {
+    if (code.includes(deepkitDistPath)) return code;
 
-const tscContent = readFileSync(join(typeScriptPath, 'tsc.js'), 'utf8');
-writeFileSync(join(typeScriptPath, 'tsc.js'), injectTransformer(deepkitDistPath, tscContent));
+    code = code.replace(/var customTransformers = (.*);/, function (a) {
+        return a + '\n    ' + getCode(deepkitDistPath, 'customTransformers');
+    });
+
+    return code;
+}
+
+const typeScriptPath = dirname(require.resolve('typescript', {paths: [cwd()]}));
+const deepkitDistPath = relative(typeScriptPath, __dirname);
+
+const tscContent = readFileSync(join(typeScriptPath, 'typescript.js'), 'utf8');
+writeFileSync(join(typeScriptPath, 'typescript.js'), injectTransformerForTypescript(deepkitDistPath, tscContent));
 
 console.log('Deepkit Type: Injected TypeScript transformer at', typeScriptPath);
--- packages/type/tests/transformer.spec.ts
+++ packages/type/tests/transformer.spec.ts
@@ -1,28 +1,121 @@
 import { describe, expect, test } from '@jest/globals';
-import { CompilerOptions, ModuleKind, ScriptTarget, transpileModule } from 'typescript';
-import { transformer } from '../src/transformer';
+import {
+    CompilerOptions,
+    createCompilerHost,
+    createProgram,
+    createSourceFile,
+    Declaration,
+    isEnumDeclaration,
+    isPropertyDeclaration,
+    ModuleKind,
+    Node,
+    ScriptKind,
+    ScriptTarget,
+    SourceFile,
+    TransformationContext,
+    TransformerFactory,
+    transpileModule,
+    visitEachChild,
+    visitNode
+} from 'typescript';
+import { DeepkitTransformer, transformer } from '../src/transformer';
 
 Error.stackTraceLimit = 200;
 
-function transpile(code: string): string {
-    const options: CompilerOptions = {
-        experimentalDecorators: true,
-        module: ModuleKind.CommonJS,
-        target: ScriptTarget.ES2020,
+const options: CompilerOptions = {
+    experimentalDecorators: true,
+    module: ModuleKind.CommonJS,
+    transpileOnly: true,
+    target: ScriptTarget.ES2020,
+};
+
+function transpile(source: string | { [file: string]: string }, useTransformer: TransformerFactory<SourceFile> = transformer) {
+    if ('string' === typeof source) {
+        return transpileModule(source, {
+            compilerOptions: options,
+            transformers: { before: [transformer] }
+        }).outputText;
+    }
+
+    const files: { [path: string]: SourceFile } = {};
+    if ('string' === typeof source) {
+        files['app.ts'] = createSourceFile('app.ts', source, ScriptTarget.ES2020, true, ScriptKind.TS);
+    } else {
+        for (const [file, src] of Object.entries(source)) {
+            files[file + '.ts'] = createSourceFile(file + '.ts', src, ScriptTarget.ES2020, true, ScriptKind.TS);
+        }
+    }
+
+    let appTs = '';
+    const host = createCompilerHost(options);
+    host.writeFile = (fileName, data, writeByteOrderMark, onError, sourceFiles) => {
+        if (fileName === 'app.js') appTs = data;
+    };
+    const originalGetSourceFile = host.getSourceFile;
+    host.getSourceFile = (fileName: string, languageVersion: ScriptTarget) => {
+        return files[fileName] || originalGetSourceFile(fileName, languageVersion);
     };
 
-    return transpileModule(code, {
-        compilerOptions: options,
-        transformers: { before: [transformer] }
-    }).outputText;
+    const program = createProgram(Object.keys(files), options, host);
+    program.emit(files['app.ts'], undefined, undefined, undefined, { before: [useTransformer] });
+    return appTs;
 }
 
-const tests: [code: string, contains: string][] = [
+test('transpile single', () => {
+    expect(transpile(`const p: string = '';`).trim()).toBe(`const p = '';`);
+});
+
+test('transpile transformer', () => {
+    let foundDeclaration: Declaration | undefined;
+    const transformer: TransformerFactory<SourceFile> = (context: TransformationContext) => {
+        const deepkitTransformer = new DeepkitTransformer(context);
+        return (sourceFile) => {
+            deepkitTransformer.sourceFile = sourceFile;
+            const visitor = (node: Node): Node => {
+                if (isPropertyDeclaration(node) && node.type) {
+                    const symbol = deepkitTransformer.findSymbol(node.type);
+                    if (symbol) {
+                        foundDeclaration = deepkitTransformer.findDeclaration(symbol);
+                    }
+                }
+                return visitEachChild(node, visitor, context);
+            };
+            return visitNode(sourceFile, visitor);
+        };
+    };
+    transpile({
+            app: `import {MyEnum} from './module'; class Controller {p: MyEnum;}`,
+            module: `export const enum MyEnum {}`
+        },
+        transformer
+    );
+    expect(foundDeclaration && isEnumDeclaration(foundDeclaration)).toBe(true);
+});
+
+const tests: [code: string | { [file: string]: string }, contains: string][] = [
+    [`import {t} from '@deepkit/type'; class Entity { @t p(): number {}}`, 'type_1.t.number'],
+    [`import {t} from '@deepkit/type'; class Entity { @t p(param: string): number {}}`, `type_1.t.number`],
+    [`import {t} from '@deepkit/type'; class Entity { @t p(param: string): number {}}`, `type_1.t.string.name('param')`],
     [`import {t} from '@deepkit/type'; class Entity { @t p: number;}`, 'type_1.t.number'],
     [`import {t} from '@deepkit/type'; class Entity { @t p?: number;}`, 'type_1.t.number.optional'],
     [`import {t} from '@deepkit/type'; class Entity { @t p: string;}`, 'type_1.t.string'],
     [`import {t} from '@deepkit/type'; class Entity { @t p: boolean;}`, 'type_1.t.boolean'],
     [`import {t} from '@deepkit/type'; class Entity { @t p: Date;}`, 'type_1.t.date'],
+    [`import {t} from '@deepkit/type'; class Entity { @t p: any;}`, 'type_1.t.any'],
+    [`import {t} from '@deepkit/type'; class Entity { @t p: bigint;}`, 'type_1.t.bigint'],
+    [`import {t} from '@deepkit/type'; class Entity { @t p: Promise<number>;}`, 'type_1.t.promise(type_1.t.number)'],
+    [`import {t} from '@deepkit/type'; class Entity { @t p: Entity<number, string>;}`, 'type_1.t.type(Entity).generic(type_1.t.number, type_1.t.string)'],
+    [`import {t} from '@deepkit/type'; class Entity { @t p: Promise<Subject<number>>;}`, 'type_1.t.promise(type_1.t.type(Subject).generic(type_1.t.number))'],
+    [`import {t} from '@deepkit/type'; class Model{}; class Entity { @t p: Partial<Model>;}`, 'type_1.t.partial(Model)'],
+    [{
+        app: `import {t} from '@deepkit/type'; import {MyEnum} from './enum'; class Entity { @t p: MyEnum;}`,
+        enum: `export enum MyEnum {}`
+    }, 'type_1.t.enum(enum_1.MyEnum)'],
+    [{
+        app: `import {t} from '@deepkit/type'; import {Model} from './model'; class Entity { @t p: Model;}`,
+        model: `export class Model {}`
+    }, 'type_1.t.type(model_1.Model)'],
+    [`import {t} from '@deepkit/type'; function() {enum MyEnum {}; class Entity { @t p: MyEnum;} }`, 'type_1.t.enum(MyEnum)'],
     [`import {t} from '@deepkit/type'; class Entity { @t p: Uint8Array;}`, 'type_1.t.type(Uint8Array)'],
     [`import {t} from '@deepkit/type'; class Entity { @t p: ArrayBuffer;}`, 'type_1.t.type(ArrayBuffer)'],
     [`import {t} from '@deepkit/type'; class Model {} class Entity { @t p: Model;}`, 'type_1.t.type(Model)'],
@@ -45,7 +138,8 @@ const tests: [code: string, contains: string][] = [
 describe('transformer', () => {
     for (const entry of tests) {
         const [code, contains] = entry;
-        test(`${contains}: ${code.slice(-40)}`, () => {
+        const label = 'string' === typeof code ? code : code['app.ts'] || '';
+        test(`${contains}: ${label.slice(-40)}`, () => {
             expect(transpile(code)).toContain(contains);
         });
     }
