49c61c783360c2f10755fb601555490a52b9f007
Marc J. Schmidt <marc@marcjschmidt.de>
2022-07-01 21:59:21 +0200

    chore: fix typedoc

--- typedoc.js
+++ typedoc.js
@@ -1,6 +1,6 @@
 module.exports = {
     out: 'docs',
-    exclude: ['**/node_modules/**', '**/*.spec.ts', '**/dist/**'],
+    exclude: ['**/node_modules/**', '**/*.spec.ts', '**/dist/**', 'packages/*-gui', 'packages/ui-library', 'packages/benchmark', 'packages/example-app', 'packages/framework-integration'],
     name: 'Deepkit',
     excludePrivate: true
 };
