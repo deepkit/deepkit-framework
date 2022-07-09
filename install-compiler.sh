6769eca39c477d8a04486e97d9e501d9b3e66257
Marc J. Schmidt <marc@marcjschmidt.de>
2022-07-09 18:51:00 +0200

    chore: fix install compiler in GUI projects

---
+++ install-compiler.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+set -e;
+
+tsc --build packages/type-compiler/tsconfig.json;
+node packages/type-compiler/dist/cjs/install-transformer.js;
+
+
+node packages/type-compiler/dist/cjs/install-transformer.js packages/api-console-gui;
+node packages/type-compiler/dist/cjs/install-transformer.js packages/orm-browser-gui;
+node packages/type-compiler/dist/cjs/install-transformer.js packages/framework-debug-gui;
