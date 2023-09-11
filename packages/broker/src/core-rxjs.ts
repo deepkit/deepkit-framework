3afc806425c41aa65aea29e410633320d8c6f376
marcus-sa <marcus-sa@outlook.com>
2023-09-11 21:34:40 +0200

    fix(broker): core-rxjs import

---
+++ packages/broker/src/core-rxjs.ts
@@ -0,0 +1,12 @@
+/*
+ * Deepkit Framework
+ * Copyright (C) 2021 Deepkit UG, Marc J. Schmidt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the MIT License.
+ *
+ * You should have received a copy of the MIT License along with this program.
+ */
+
+// FIXME: 'rootDir' is expected to contain all source files.
+export * from '../../../dist/packages/core-rxjs';
