5354e5d13bd83beb1c375c677f6f9a7e1c39699a
marcus-sa <marcus-sa@outlook.com>
2023-09-11 16:08:47 +0200

    build(event): fix circular peer dependency

--- packages/rollup.config.js
+++ packages/rollup.config.js
@@ -34,6 +34,14 @@ module.exports = (config) => {
         config.plugins.push(nxResolvePackages(packages));
     }
 
+    /*return {
+        ...config,
+        output: {
+            ...config.output,
+            sourcemap: true,
+        }
+    };*/
+
     return config;
 }
 
