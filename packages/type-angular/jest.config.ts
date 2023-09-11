37c7087f33f569dfbf3d9cb95cd55d3427c030db
marcus-sa <marcus-sa@outlook.com>
2023-09-11 14:12:47 +0200

    build(type-angular): migrate to nx

---
+++ packages/type-angular/jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'jest';
+
+export default {
+    displayName: 'type-angular',
+    preset: '../../jest.preset.js',
+    testEnvironment: 'node',
+    transform: {
+        '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+    },
+    moduleFileExtensions: ['ts', 'js', 'html'],
+    coverageDirectory: '../../coverage/packages/type-angular',
+} as Config;
--- packages/type-angular/src/index.ts
+++ packages/type-angular/src/index.ts
@@ -8,4 +8,4 @@
  * You should have received a copy of the MIT License along with this program.
  */
 
-export * from './src/form.js';
+export * from './lib/form.js';
