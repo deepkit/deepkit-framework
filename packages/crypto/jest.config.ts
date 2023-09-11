93b2034f7695115952520c12dd5225327584853f
marcus-sa <marcus-sa@outlook.com>
2023-09-12 00:07:30 +0200

    test(crypto): use correct name

--- packages/crypto/jest.config.ts
+++ packages/crypto/jest.config.ts
@@ -1,12 +1,12 @@
 import type { Config } from 'jest';
 
 export default {
-    displayName: 'core',
+    displayName: 'crypto',
     preset: '../../jest.preset.js',
     testEnvironment: 'node',
     transform: {
         '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
     },
     moduleFileExtensions: ['ts', 'js', 'html'],
-    coverageDirectory: '../../coverage/packages/core',
+    coverageDirectory: '../../coverage/packages/crypto',
 } as Config;
