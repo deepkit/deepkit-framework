ad1b4a7e63e653f0b55e190d08ad96aaf9eefa79
marcus-sa <marcus-sa@outlook.com>
2023-09-11 13:17:55 +0200

    build(orm-browser): migrate to nx

---
+++ packages/orm-browser/jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'jest';
+
+export default {
+    displayName: 'orm-browser',
+    preset: '../../jest.preset.js',
+    testEnvironment: 'node',
+    transform: {
+        '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+    },
+    moduleFileExtensions: ['ts', 'js', 'html'],
+    coverageDirectory: '../../coverage/packages/orm-browser',
+} as Config;
