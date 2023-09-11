947f09b89ff3e0193a3f0b3dd3de681e72af3cc8
marcus-sa <marcus-sa@outlook.com>
2023-09-11 12:57:12 +0200

    build(fs): migrate to nx

---
+++ packages/fs/jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'jest';
+
+export default {
+    displayName: 'fs',
+    preset: '../../jest.preset.js',
+    testEnvironment: 'node',
+    transform: {
+        '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+    },
+    moduleFileExtensions: ['ts', 'js', 'html'],
+    coverageDirectory: '../../coverage/packages/fs',
+} as Config;
