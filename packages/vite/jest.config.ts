d158e62148eba0bf014a1f5938a7bd94d7953899
marcus-sa <marcus-sa@outlook.com>
2023-09-11 14:15:04 +0200

    build(vite): migrate to nx

---
+++ packages/vite/jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'jest';
+
+export default {
+    displayName: 'vite',
+    preset: '../../jest.preset.js',
+    testEnvironment: 'node',
+    transform: {
+        '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+    },
+    moduleFileExtensions: ['ts', 'js', 'html'],
+    coverageDirectory: '../../coverage/packages/vite',
+} as Config;
