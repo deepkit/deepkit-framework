84fbdce6b70f887cf17ab554fc5ec9a4d037d7db
marcus-sa <marcus-sa@outlook.com>
2023-09-11 11:32:08 +0200

    build: remove peer-deps-external rollup plugin
    peer-deps-external conflicts with nx-resolve-packages when @deepkit packages are specified in package.json peerDependencies

--- packages/rollup.config.js
+++ packages/rollup.config.js
@@ -4,41 +4,37 @@ const { workspaceRoot } = require('@nx/devkit');
 const tsconfig = require(path.join(workspaceRoot, 'tsconfig.base.json'));
 
 const nxResolvePackages = (packages) => {
-    const cache = new Map();
-
     return {
         name: 'nx-resolve-packages',
-        resolveId: async (importee) => {
-            if (cache.has(importee)) return cache.get(importee);
+        resolveId: async (id) => {
+            if (!packages.includes(id)) return null;
 
-            if (!packages.includes(importee)) return null;
-
-            const projectName = importee.split('/')[1];
+            const projectName = id.split('/')[1];
 
             const projectPath = path.join(workspaceRoot, 'dist/packages', projectName);
 
-            const result = {
+            return {
                 id: projectPath,
                 external: true
             }
-
-            cache.set(importee, result);
-
-            return result;
-        },
+        }
     }
 }
 
 module.exports = (config) => {
-    // we can't require @deepkit/* packages during development
     if (process.env.NODE_ENV === 'development') {
-        const external = config.external;
         const packages = Object.keys(tsconfig.compilerOptions.paths);
-        config.external = (id) => {
-            if (packages.includes(id)) return false;
-            return external(id)
-        }
+
+        const external = config.external;
+        config.external = (id) => packages.includes(id) ? false : external(id);
+
+        // peer-deps-external conflicts with nx-resolve-packages when @deepkit packages are specified in package.json peerDependencies
+        config.plugins = config.plugins.filter(plugin => plugin.name !== 'peer-deps-external');
+
         config.plugins.push(nxResolvePackages(packages));
     }
+
     return config;
 }
+
+module.exports.nxResolvePackages = nxResolvePackages
