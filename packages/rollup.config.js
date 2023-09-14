50491bd141a1c35670920d02a188e15b3ff81571
marcus-sa <marcus-sa@outlook.com>
2023-09-14 11:51:37 +0200

    build: resolve dist package paths in emitted declaration files

--- packages/rollup.config.js
+++ packages/rollup.config.js
@@ -5,25 +5,50 @@ const preserveShebang = require('rollup-plugin-preserve-shebang');
 const tsconfig = require(path.join(workspaceRoot, 'tsconfig.base.json'));
 
 const nxResolveDistPackages = (packages) => {
+    const resolvePackagePath =  (id) => {
+        const projectName = id.split('/')[1];
+
+        const projectPath = path.join(workspaceRoot, 'packages', projectName);
+
+        const pkg = require(path.join(projectPath, 'package.json'));
+
+        if (pkg.name !== id && !id.startsWith(`${pkg.name}/`)) {
+            throw new Error(`Package name doesn't match, expected ${id} but got ${pkg.name}`);
+        }
+
+        return path.join(workspaceRoot, 'dist/packages', projectName);
+    }
+
     return {
         name: 'nx-resolve-dist-packages',
         resolveId: async (id) => {
             if (!packages.includes(id)) return null;
 
-            const projectName = id.split('/')[1];
-
-            const projectPath = path.join(workspaceRoot, 'dist/packages', projectName);
-            const pkg = require(path.join(projectPath, 'package.json'));
-
-            if (pkg.name !== id) {
-                throw new Error(`Package name doesn't match, expected ${id} but got ${pkg.name}`);
-            }
+            const projectPath = resolvePackagePath(id);
 
             return {
                 id: projectPath,
                 external: true
             }
-        }
+        },
+        generateBundle(options, bundle) {
+            for (const info of Object.values(bundle)) {
+                if (!info.fileName.endsWith('.d.ts') || info.type !== 'asset') continue;
+
+                const source = typeof info.source !== 'string' ? new TextDecoder().decode(info.source) : info.source;
+
+                const newSource = packages.reduce((source, id) => {
+                    const distPath = resolvePackagePath(id);
+                    if (!distPath) return source;
+                    return source.replaceAll(id, distPath);
+                }, source);
+
+                this.emitFile({
+                    ...info,
+                    source: newSource,
+                })
+            }
+        },
     }
 }
 
