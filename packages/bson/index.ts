138a11e1105e1963b002b7aeec5500c012569d26
Marcus S. Abildskov <8391194+marcus-sa@users.noreply.github.com>
2023-10-01 18:54:58 +0200

    fix(bson): import named __ΩBSONDeserializer not found in module (#472)
    Signed-off-by: Marcus S. Abildskov <8391194+marcus-sa@users.noreply.github.com>

--- packages/bson/index.ts
+++ packages/bson/index.ts
@@ -8,9 +8,8 @@
  * You should have received a copy of the MIT License along with this program.
  */
 
-export { ObjectId } from './src/model.js';
-export { deserializeBSONWithoutOptimiser, ParserV2 as Parser } from './src/bson-parser.js';
-export { getBSONDeserializer, deserializeBSON, BSONDeserializer } from './src/bson-deserializer.js';
-export {
-    stringByteLength, BSONSerializer, createBSONSizer, getBSONSizer, getBSONSerializer, bsonBinarySerializer, serializeBSON, serializeWithoutOptimiser, Writer, BSONBinarySerializer, ValueWithBSONSerializer
-} from './src/bson-serializer.js';
+export * from './src/model.js';
+export * from './src/bson-parser.js';
+export { ParserV2 as Parser } from './src/bson-parser.js';
+export * from './src/bson-deserializer.js';
+export * from './src/bson-serializer.js';
