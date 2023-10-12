5cb33cfd7b98c3549ff30c42980c3d6e8f65a447
Marc J. Schmidt <marc@marcjschmidt.de>
2023-10-12 20:47:04 +0200

    fix(framework): support esm environment for debug gui http routes

--- packages/framework/src/debug/http-debug.controller.ts
+++ packages/framework/src/debug/http-debug.controller.ts
@@ -10,12 +10,15 @@
 
 import { registerStaticHttpController } from '@deepkit/http';
 import { AppModule, findParentPath } from '@deepkit/app';
+import { dirname } from 'path';
+import { getCurrentFileName } from '@deepkit/core';
 
 export function registerDebugHttpController(module: AppModule<any>, path: string): void {
-    const localPath = findParentPath('node_modules/@deepkit/framework-debug-gui/dist/framework-debug-gui', __dirname);
+    const currentDir = dirname(getCurrentFileName());
+    const localPath = findParentPath('node_modules/@deepkit/framework-debug-gui/dist/framework-debug-gui', currentDir);
     if (localPath) {
-        registerStaticHttpController(module, {path, localPath, groups: ['app-static'], controllerName: 'FrameworkDebuggerController'});
+        registerStaticHttpController(module, { path, localPath, groups: ['app-static'], controllerName: 'FrameworkDebuggerController' });
     } else {
-        console.log('Warning: node_modules/@deepkit/framework-debug-gui no build found in ' + __dirname);
+        console.log('Warning: node_modules/@deepkit/framework-debug-gui no build found in ' + currentDir);
     }
 }
