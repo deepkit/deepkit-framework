a59df269c5ff58dcff98b465a1e181fe84d426a2
Alexander Bolshakov <alpharder.mail@gmail.com>
2023-10-20 08:29:11 +0300

    Angular apps are working now

--- install-compiler.sh
+++ install-compiler.sh
@@ -6,6 +6,7 @@ tsc --build packages/type-compiler/tsconfig.json;
 node packages/type-compiler/dist/cjs/install-transformer.js;
 
 
+node packages/type-compiler/dist/cjs/install-transformer.js packages/desktop-ui;
 node packages/type-compiler/dist/cjs/install-transformer.js packages/api-console-gui;
 node packages/type-compiler/dist/cjs/install-transformer.js packages/orm-browser-gui;
 node packages/type-compiler/dist/cjs/install-transformer.js packages/framework-debug-gui;
