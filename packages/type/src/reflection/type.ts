53216ff1893a7933451a3441157e6df1538e99ac
Marcus S. Abildskov <8391194+marcus-sa@users.noreply.github.com>
2024-02-09 13:22:40 +0100

    feat(type): allow type annotation options to be extracted
    Signed-off-by: Marcus S. Abildskov <8391194+marcus-sa@users.noreply.github.com>

--- packages/type/src/reflection/type.ts
+++ packages/type/src/reflection/type.ts
@@ -1548,7 +1548,9 @@ export interface EntityOptions {
  * const param1 = typeToObject(data[0]); //yes
  * ```
  */
-export type TypeAnnotation<T extends string, Options = never> = { __meta?: never & [T, Options] };
+export type TypeAnnotation<T extends string, Options = never> = { __meta?: never | [T, Options] };
+
+export type ExtractAnnotationOptions<T extends TypeAnnotation<string, any>> = Exclude<NonNullable<T['__meta']>, never>[1];
 
 /**
  * Type to decorate an interface/object literal with entity information.
